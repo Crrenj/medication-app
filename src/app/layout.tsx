db1be52887238caf614753c7bdb96aa2c6e8051f
crrenj <juanlantiguajorge@gmail.com>
2024-12-09 13:19:05 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +26,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ClerkProvider>
+          <html lang="en">
+            <body>
+              <header>
+                
+              </header>
+              <main>{children}</main>
+            </body>
+          </html>
+        </ClerkProvider>
       </body>
     </html>
   );
